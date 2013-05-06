966a03d7e3f826991c43bf2f66288a2e1aacfc0a
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-06 23:48:20 +0400

    Добавил состояния всем работающим пунктам меню

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def active_class(page = {})
+    current_controller = true if params[:controller] == page[:controller] || page[:controller].nil?
+    current_action = true if page[:action].include?(params[:action]) || page[:action].nil?
+    return "active" if current_controller && current_action
+    return ""
+  end
+  
+  def create_class
+    return "toDo" if current_user.flashcards.created_on(session[:date]).size < current_user.daily_limit
+    return ""
+  end
+  
+  def repeat_class
+    return "toDo" if current_user.repetitions.planned.for(session[:date]).size > 0
+    return "inactive"
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,5 +1,2 @@
 module StaticPagesHelper
-  def active_class(name)
-    "class=active" if params[:action] == name
-  end
 end
