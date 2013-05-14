d3920ea33af187be16e471de498128670042d4c3
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-14 22:33:57 +0400

    Решена проблема мигания меню

--- app/assets/javascripts/menu_transform_script.js
+++ app/assets/javascripts/menu_transform_script.js
@@ -1,5 +1,7 @@
+$('head').append('<link rel="stylesheet" media="all" href="/stylesheets/invisible_menu.css">');
+
 $(function() {
-	var dropdownMenu = $("#dropdownMenu");
+	var dropdownMenu = $("#dropdownMenu").css('visibility', 'visible');
 	try {
 		// Этот код находится в try-catch, потому что в IE8 insertAfter вызывает ошибку (почему - пока не выяснил). Если ошибку не отлавливать, будет выполняться append, таким образом, меню будет исчезать. А в collapse в IE8 меню всё равно не нужно засовывать, потому что в этом браузере сайт фиксированной ширины (браузер не поддерживат media qeuries).
 		var menuDiv = $("<div class = 'nav-collapse collapse'>").insertAfter($("#elementBeforeDropdown"));
