c21ba4922ff8719e1f8f9126a9550d8559b0cd9c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-15 15:04:58 +0400

    Добавил индекс к полю email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331111445) do
+ActiveRecord::Schema.define(:version => 20130515110040) do
 
   create_table "flashcards", :force => true do |t|
     t.integer  "user_id"
@@ -42,5 +42,7 @@ ActiveRecord::Schema.define(:version => 20130331111445) do
     t.datetime "created_at",                                        :null => false
     t.datetime "updated_at",                                        :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
