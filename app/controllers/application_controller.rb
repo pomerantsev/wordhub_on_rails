f2449580d8d14442b855f6b51666f1da1c90cef7
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-15 17:30:31 +0400

    Рефакторинг application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :adjust_current_date
   
   helper_method :current_user
   helper_method :logged_in
@@ -13,24 +15,32 @@ class ApplicationController < ActionController::Base
   def logged_in
     session[:user_id]
   end
+
+
   
   protected
   
-
-  
   def confirm_logged_in
-    unless logged_in
-      redirect_to root_url
-      return false # halts the before_filter
-    else
-      session[:date] = Date.today if !session[:date] || session[:date] < Date.today
+    if logged_in
       current_user.repetitions.adjust_dates(session[:date])
       return true
+    else
+      redirect_to root_url
+      return false
     end
   end
-  
+
   def current_user
     User.find_by_id session[:user_id]
   end  
+
+
+
+  private
+
+  # Чтобы всё приложение вычисляло дату единым образом, используется одна переменная сессии.
+  def adjust_current_date
+      session[:date] = Date.today
+  end
   
 end
