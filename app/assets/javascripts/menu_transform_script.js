32f52beb4ce96d80748480188ccb7245e4f7ad96
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-15 15:36:59 +0400

    Вместо дополнительного файла добавил стиль для начального скрытия меню непосредственно в шапку

--- app/assets/javascripts/menu_transform_script.js
+++ app/assets/javascripts/menu_transform_script.js
@@ -1,9 +1,9 @@
 $('html').removeClass('no-js').addClass('js');
 
-$('head').append('<link rel="stylesheet" media="all" href="/stylesheets/invisible_menu.css">');
+$('head').append('<style> #dropdownMenu { display: none; } </style>');
 
 $(function() {
-	var dropdownMenu = $("#dropdownMenu").css('visibility', 'visible');
+	var dropdownMenu = $("#dropdownMenu").show();
 	try {
 		// Этот код находится в try-catch, потому что в IE8 insertAfter вызывает ошибку (почему - пока не выяснил). Если ошибку не отлавливать, будет выполняться append, таким образом, меню будет исчезать. А в collapse в IE8 меню всё равно не нужно засовывать, потому что в этом браузере сайт фиксированной ширины (браузер не поддерживат media qeuries).
 		var menuDiv = $("<div class = 'nav-collapse collapse'>").insertAfter($("#elementBeforeDropdown"));
