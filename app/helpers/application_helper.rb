d5927d6b3ff972eced9bde29f9437aa108e91c2f
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-05-17 00:54:28 +0400

    Форматированный вывод ошибок и комментарии к application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,42 @@
 module ApplicationHelper
+
+  # Хелпер для определения, активен ли определённый пункт меню.
   def active_class(page = {})
     current_controller = true if params[:controller] == page[:controller] || page[:controller].nil?
     current_action = true if page[:action].include?(params[:action]) || page[:action].nil?
     return "active" if current_controller && current_action
     return ""
   end
+
   
+  # Класс для выделения цветом пункта меню "Создать", если на сегодня ещё не все карточки созданы.
   def create_class
     return "toDo" if current_user.flashcards.created_on(current_date).size < current_user.daily_limit
     return ""
   end
+
   
+  # Класс для выделения цветом пункта меню "Повторить" в зависимости от того, всё ли запланированное на сегодня повторено.
   def repeat_class
     return "toDo" if current_user.repetitions.planned.for(current_date).size > 0
     return "inactive"
   end
 
+
+  # Метод для дебага.
   def planned_repetitions_count_by_date
     Hash[current_user.repetitions.planned_count_by_date.sort] if current_user.present?
   end
 
+
+  # Хелпер для вывода сообщения об ошибке сохранения в базу.
   def errors(object)
-    object.errors.full_messages.inspect if object.present?
+    if object.present?
+      message = "<ul>".html_safe
+      object.errors.full_messages.each do |msg|
+        message += "<li>#{msg}</li>".html_safe
+      end
+      return message + "</ul>".html_safe
+    end
   end
 end
