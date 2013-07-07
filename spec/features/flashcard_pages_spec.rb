2205f2c50c6244797dbad8d05d7b5ce653f6a60b
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-07-07 08:59:51 +0400

    Добавил тест с JS

--- spec/features/flashcard_pages_spec.rb
+++ spec/features/flashcard_pages_spec.rb
@@ -21,11 +21,10 @@ feature "Flashcards" do
 		expect(page).to_not have_link "Показать удалённые карточки"
 	end
 
-	scenario "deleting and undeleting a flashcard" do
+	scenario "deleting and undeleting a flashcard without JS" do
 		flashcard = user.flashcards.first
 		visit edit_flashcard_path(flashcard)
 		click_button "Удалить"
-		# click_link "Показать удалённые карточки"
 			
 		expect(page).to have_selector("label", text: first_line(flashcard.front_text))
 		# Раньше xpath записывался как "//input[@value='Восстановить' and @disabled]", но с @disabled тест будет работать только при включённом JS.
@@ -37,4 +36,21 @@ feature "Flashcards" do
 		expect(page).to have_link flashcard.front_text, href: edit_flashcard_path(flashcard)
 		expect(page).to_not have_button "Восстановить"
   end
+
+  scenario "deleting and undeleting a flashcard with JS", js: true do
+  	flashcard = user.flashcards.first
+		visit edit_flashcard_path(flashcard)
+		click_button "Удалить"
+		click_link "Показать удалённые карточки"
+			
+		expect(page).to have_selector("label", text: first_line(flashcard.front_text))
+		expect(page).to have_xpath "//input[@value='Восстановить' and @disabled]"
+
+		check first_line(flashcard.front_text)
+		click_button "Восстановить"
+
+		expect(page).to have_link flashcard.front_text, href: edit_flashcard_path(flashcard)
+		expect(page).to_not have_button "Восстановить"
+  end
+
 end
