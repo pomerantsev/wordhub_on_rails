28b136328e26cd61763e7509a009df629bbe7578
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-08-10 23:43:55 +0400

    Refactor user stats template

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+  def total_stats(stats)
+    [{ name: "Всего карточек:",
+       value: stats[:total_flashcards] },
+     { name: "Из них выучено:",
+       value: stats[:learned_flashcards] },
+     { name: "Всего запланировано повторов:",
+       value: stats[:total_planned_repetitions] },
+     { name: "Ближайшие повторы:",
+       value: "#{stats[:planned_repetitions_for_nearest_date]} (#{stats[:nearest_date]})" },
+     { name: "Запланировано повторов до:",
+       value: stats[:last_date_with_planned_repetitions] }
+    ]
+  end
+
+  def stats_for_period(stats)
+    [{ name: "Создано:",
+       value: stats[:created] },
+     { name: "Выучено:",
+       value: stats[:learned] },
+     { name: "Всего повторов:",
+       value: stats[:repetitions_run] },
+     { name: "Из них успешных:",
+       value: "#{stats[:successful_repetitions]} (#{stats[:successful_repetitions_percentage]}%)" }
+     ]
+  end
 end
