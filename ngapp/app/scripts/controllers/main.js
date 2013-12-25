01b961aef3143f0d4b888a3d77d26e13cb90dbaf
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-25 23:11:52 +0400

    Initial app generated with Yeoman

---
+++ ngapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wordhubApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
