1f9eec3239f94da0395d821ec5ee34ca6c359e49
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-28 21:28:46 +0400

    Output data from the server

--- ngapp/app/scripts/controllers/flashcardsIndex.js
+++ ngapp/app/scripts/controllers/flashcardsIndex.js
@@ -1,5 +1,12 @@
 'use strict';
 
-app.controller('FlashcardsIndexCtrl', function () {
-  this.flashcards = [{frontText: 'Hello', backText: 'World'}];
+app.controller('FlashcardsIndexCtrl', function ($http) {
+  var that = this;
+  $http.get('/api/users.json')
+    .success(function (data) {
+      that.flashcards = [];
+      angular.forEach(data, function (user) {
+        that.flashcards.push({frontText: user.email, backText: user.name});
+      });
+    });
 });
--- ngapp/app/scripts/controllers/main.js
+++ ngapp/app/scripts/controllers/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
 angular.module('wordhubApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/users.json')
-      .success(function (data) {
-        $scope.users = data;
-      });
+  .controller('MainCtrl', function () {
   });
