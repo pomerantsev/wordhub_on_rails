3aa6dfcd59b33fd2512851e761ecd55941a2fe4f
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-03 22:52:37 +0400

    Use transformRequest to properly name flashcard params

--- ngapp/app/scripts/controllers/newFlashcard.js
+++ ngapp/app/scripts/controllers/newFlashcard.js
@@ -4,6 +4,6 @@ angular.module('wordhubApp')
   .controller('NewFlashcardCtrl', function (Flashcard) {
     this.flashcard = {};
     this.create = function () {
-      Flashcard.save({flashcard: this.flashcard});
+      Flashcard.save(this.flashcard);
     };
   });
--- ngapp/app/scripts/services/flashcard.js
+++ ngapp/app/scripts/services/flashcard.js
@@ -2,5 +2,18 @@
 
 angular.module('wordhubApp')
   .factory('Flashcard', function ($resource) {
-    return $resource('/api/flashcards/:id.json');
+    return $resource('/api/flashcards/:id.json', {}, {
+      save: {
+        method: 'POST',
+        transformRequest: function (data) {
+          data = {
+            flashcard: {
+              front_text: data.frontText,
+              back_text: data.backText
+            }
+          };
+          return JSON.stringify(data);
+        }
+      }
+    });
   });
