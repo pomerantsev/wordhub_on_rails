e1487c6be372fd55a7285c59c8df8cc14399219a
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-03 23:02:45 +0400

    Handle success and error in create flashcard action

--- ngapp/app/scripts/controllers/newFlashcard.js
+++ ngapp/app/scripts/controllers/newFlashcard.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('wordhubApp')
-  .controller('NewFlashcardCtrl', function (Flashcard) {
-    this.flashcard = {};
-    this.create = function () {
-      Flashcard.save(this.flashcard);
+  .controller('NewFlashcardCtrl', function (Flashcard, SETTINGS, $rootScope) {
+    var ctrl = this;
+    ctrl.flashcard = {};
+    ctrl.create = function () {
+      Flashcard.save(ctrl.flashcard).$promise
+        .then(function () {
+          ctrl.flashcard = {};
+        }, function (reason) {
+          $rootScope.$broadcast(SETTINGS.customErrorEvent, reason.data);
+        });
     };
   });
