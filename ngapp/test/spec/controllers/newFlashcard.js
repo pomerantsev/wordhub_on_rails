baf5521cdbef347cf5dff0590617f70667b54bb7
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-04 18:07:58 +0400

    Add tests for NewFlashcardCtrl

--- ngapp/test/spec/controllers/newFlashcard.js
+++ ngapp/test/spec/controllers/newFlashcard.js
@@ -1,19 +1,43 @@
 'use strict';
 
-describe('Controller: NewflashcardCtrl', function () {
+describe('Controller: NewFlashcardCtrl', function () {
 
   // load the controller's module
   beforeEach(module('wordhubApp'));
 
-  var NewflashcardCtrl,
-    scope;
+  var newFlashcardCtrl, $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    NewflashcardCtrl = $controller('NewflashcardCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function ($controller, _$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+    newFlashcardCtrl = $controller('NewFlashcardCtrl');
   }));
+
+  describe('#create', function () {
+    function sharedBehaviorForCreate (flashcard, status) {
+      beforeEach(function () {
+        $httpBackend.expectPOST('/api/flashcards.json').respond(status);
+        newFlashcardCtrl.flashcard = flashcard;
+        newFlashcardCtrl.create();
+        $httpBackend.flush();
+      });
+    }
+    describe('when successful', function () {
+      var flashcard = {frontText: 'front', backText: 'back'};
+      sharedBehaviorForCreate(flashcard, 201);
+
+      it('empties the controller\'s flashcard object', function () {
+        expect(newFlashcardCtrl.flashcard).toEqual({});
+      });
+    });
+    describe('when unsuccessful', function () {
+      var flashcard = {frontText: 'front', backText: ''};
+      sharedBehaviorForCreate(flashcard, 422);
+
+      it('doesn\'t empty the flashcard object', function () {
+        expect(newFlashcardCtrl.flashcard).not.toEqual({});
+      });
+    });
+  });
 
 });
