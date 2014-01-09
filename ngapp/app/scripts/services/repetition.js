116dea2bf08744f678d7cabe8a0288711e9614ee
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-09 19:54:35 +0400

    Store repetitions inside the service

--- ngapp/app/scripts/services/repetition.js
+++ ngapp/app/scripts/services/repetition.js
@@ -2,10 +2,18 @@
 
 angular.module('wordhubApp')
   .factory('Repetition', function ($resource, $rootScope, $q) {
+    var repetitionStore;
+
     var resource = $resource('/api/repetitions/:id.json', {id: '@id'}, {
       patch: {
         method: 'PATCH',
         transformRequest: function (data) {
+          // Delete the repetition from the store
+          angular.forEach(repetitionStore, function (repetition, index) {
+            if (repetition.id === data.id) {
+              repetitionStore.splice(index, 1);
+            }
+          });
           data = {
             repetition: {
               successful: data.successful
@@ -22,8 +30,23 @@ angular.module('wordhubApp')
       }
     });
 
+    var repetitionsPromise = function () {
+      if (repetitionStore) {
+        var defer = $q.defer();
+        defer.resolve(repetitionStore);
+        return defer.promise;
+      } else {
+        return resource.query().$promise
+          .then(function (data) {
+            console.log(data);
+            repetitionStore = data;
+            return data;
+          });
+      }
+    };
+
     resource.getRandom = function () {
-      return resource.query().$promise
+      return repetitionsPromise()
         .then(function (data) {
           if (data.length === 0) {
             return $q.reject();
